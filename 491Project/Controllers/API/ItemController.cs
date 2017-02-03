b0da8961d6b596a2a16a54d0d62a7492403fd205
Melody Tribble <mjtribble2@gmail.com>
2017-02-03 12:26:59 -0700

    Start of Assignment 1, not yet tested.

---
+++ 491Project/Controllers/API/ItemController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+
+namespace _491Project.Controllers.API
+{
+    public class ItemController :ApiController
+    {
+        [HttpGet]
+        [ResponseType(typeof(int))]
+        public async Task<IHttpActionResult> GetItem(int id)
+        {
+            int result = id * 10;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(List<>))]
+        public async Task<IHttpActionResult> GetItemList()
+        {
+            List<string> list = new List<string>
+            {
+                "Hi", "Hello.", "Hi", "Hello", "How", "are", "you?"
+            };
+            return Ok();
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> CreateItem([FromBody] string payload)
+        {
+            return CreatedAtRoute(
+                "DefaultApi",
+                new
+                {
+                    id = 1
+
+                }, 
+           $"create new item from {payload}" );
+        }
+
+        [HttpPatch]
+        public async Task<IHttpActionResult> UpdateItem([FromBody]int id, string payload)
+        {
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteItem(int id)
+        {
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ 491Project/Models/DB2/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _491Project.Models.DB2
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("DefautConnection")
+        {
+
+        }
+    }
+}
