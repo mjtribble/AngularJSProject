2938e8f976e0d64a575d76f14dfa726c440d2c88
Melody Tribble <mjtribble2@gmail.com>
2017-04-10 20:39:55 -0600

    Broken js update for Jack

---
+++ 491Project/Scripts/app/Services.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    angular
+        .module("MyApp.Services", ["ngResource"])
+        .factory
+        ("MyApp.Services",
+        ["$resource",
+            function ($resource) {
+                return $resource
+                (
+                    "http://msucs491-spring17-8d42d07c-01e3-4d92-a661-46d9fa7700bb.azurewebsites.net/api/item/:itemId",
+                    { itemId: '@id' },
+                    {
+                        query:
+                        {
+                            method: "GET",
+                            isArray: false
+                        }
+                    }
+                );
+            }
+        ]
+        )
+})();
--- 491Project/Scripts/app/app.js
+++ 491Project/Scripts/app/app.js
@@ -3,59 +3,44 @@
         function ($scope, $http, $resource) {
             $scope.notes = [];
             $scope.results = [];
-            $scope.item = null;
+            $scope.one_item = null;
+            $scope.items = [];
+
             $scope.locations = [
                 {id: 0,label: "Refrigerator"},
                 {id: 1,label: "Outdoor Fridge"},
                 {id: 2,label: "Cabinet A"},
                 {id: 3,label: "Pantry"}
             ];
+
             $scope.items = [
                 {id: 1, name: "Milk", description: "Cow juice", quantity: 1, expires: "2.26.17 7:00AM", location: "Refrigerator"},
                 {id: 1, name: "Milk", description: "Cow juice", quantity: 1, expires: "2.26.17 7:00AM", location: "Refrigerator"},
                 {id: 1, name: "Milk", description: "Cow juice", quantity: 1, expires: "2.26.17 7:00AM", location: "Refrigerator"},
                 {id: 1, name: "Milk", description: "Cow juice", quantity: 1, expires: "2.26.17 7:00AM", location: "Refrigerator"}
             ];
+
             $scope.notes.push("Calling...");
 
-            var ItemService = $resource('https://msucs491-spring17-assignment2.azurewebsites.net/api/item/:itemId', 
-                { itemID:'@ID'}, 
+           var ItemService = $resource('https://msucs491-spring17-assignment2.azurewebsites.net/api/item/:itemId', 
+                { itemId:'@ID'}, 
                 {
                     'get': {method:'GET'},
-                    'query': {method:'GET', isArray:false },
-                    'save': { method: 'PATCH' },
+                    'query': {method:'GET', isArray:false},
+                    'save': {method: 'PATCH' },
                     'remove': {method: 'DELETE'},
                 });
-            $scope.results = ItemService.query();
-            console.log($scope.results);
+
+            //$scope.results = ItemService.query();
+            //console.log($scope.results);
 
             $scope.notes.push("Fetching 24");
 
-            $scope.item = ItemService.get({itemID:24},
+            $scope.one_item = ItemService.get({itemId:24},
                 function() {
                     $scope.notes.push("Updating 24");
-                    $scope.item.Quantity = $scope.item.Quantity + 1;//increment by 1
+                    $scope.one_item.Quantity = $scope.one_item.Quantity + 1;//increment by 1
                     $scope.notes.push("Saving Item ID 24");
-                    $scope.item.$save(); //Save
+                    $scope.one_item.$save(); //Save
                 });
-
-            //$http({
-            //    method: 'GET',
-            //    url: 'https://randomuser.me/api/?results=10'
-            //}).then(function successCallback(response) {
-            //    $scope.notes.push("In Success Callback");
-            //    $scope.results = response.data.results;
-            //}, function errorCallback(response) {
-            //    $scope.notes.push("In Error Callback");
-            //    $scope.results = [];
-            //});
-
         }]);
-
-//$scope.$watch("filter.One + filter.Two",
-//    function (newVal, oldVal) {
-//        if (newVal == "FooBar") {
-//            alert('Filters are set to foobar')
-//        }
-//    });
-
